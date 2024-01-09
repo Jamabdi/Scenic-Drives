f7491bfeaa8ef47bbeaf68ae0feb8bd7cb3602b6
Abdirahman Jama <abdirahman.jama13@gmai.com>
2024-01-09 14:56:27 -0600

    added get route

--- server/routes/template.router.js
+++ server/routes/template.router.js
@@ -7,7 +7,19 @@ const router = express.Router();
  */
 router.get('/', (req, res) => {
   // GET route code here
+  let queryText = 'SELECT * FROM "routes" ORDER BY "id";';
+  pool.query(queryText).then((result) => {
+      res.send(result.rows);
+  })
+  .catch((error) => {
+      console.log('Error in GET /treats', error)
+      res.sendStatus(500);
+  })
 });
+
+
+  
+
 
 /**
  * POST route template
