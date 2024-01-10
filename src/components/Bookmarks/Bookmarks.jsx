069277c006ea422d1b08e5677751449d4a79aab5
Abdirahman Jama <abdirahman.jama13@gmai.com>
2024-01-10 10:22:55 -0600

    added bookmark component for implementation

---
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+
+// Function Bookmark ({ routeId, }) {
+//     const [isBookmarked, setIsBookmarked] = useState(false);
+  
+//     const userId = auth.currentUser ? auth.currentUser.uid : null;
+  
+//     useEffect(() => {
+//     }}
+
